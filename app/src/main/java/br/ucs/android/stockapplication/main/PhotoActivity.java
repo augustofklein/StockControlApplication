29954f6d668bf9d8bc895cce19591d3ac29ec5ea
augustofklein <augusto_klein@hotmail.com>
2023-07-10 09:52:39 -0300

    Ajustes gerais no layout da leitura dos itens.

--- app/src/main/java/br/ucs/android/stockapplication/main/PhotoActivity.java
+++ app/src/main/java/br/ucs/android/stockapplication/main/PhotoActivity.java
@@ -1,81 +1,25 @@
 package br.ucs.android.stockapplication.main;
 
-import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
-
 import com.google.android.material.button.MaterialButton;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
-
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-
 import br.ucs.android.stockapplication.R;
 import br.ucs.android.stockapplication.model.Capture;
 
 public class PhotoActivity extends AppCompatActivity {
-
-    private final int GALERIA_IMAGENS = 1;
-    private final int PERMISSAO_REQUEST = 2;
-    private final int CAMERA = 3;
-    private File arquivoFoto = null;
-    private ImageView imagem;
-    private ImageView barcodeImage;
     private MaterialButton btnScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
-
-        this.imagem = (ImageView) findViewById(R.id.pv_image);
-
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
-            } else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                        PERMISSAO_REQUEST);
-            }
-        }
-
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            } else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSAO_REQUEST);
-            }
-        }
 
         btnScan = findViewById(R.id.btnScan);
 
@@ -92,12 +36,6 @@ public class PhotoActivity extends AppCompatActivity {
                 intentIntegrator.initiateScan();
             }
         });
-    }
-
-    public void buscar(View view) {
-        Intent intent = new Intent(Intent.ACTION_PICK,
-                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(intent, GALERIA_IMAGENS);
     }
 
     @Override
@@ -123,55 +61,5 @@ public class PhotoActivity extends AppCompatActivity {
         }else{
             Toast.makeText(this, "OOOPS", Toast.LENGTH_SHORT).show();
         }
-
     }
-
-    private void mostraFoto(String caminho) {
-        Bitmap bitmap =
-                BitmapFactory.decodeFile(caminho);
-        imagem.setImageBitmap(bitmap);
-    }
-
-    public void tirarFoto(View view) {
-        Intent takePictureIntent = new
-                Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            try {
-                arquivoFoto = criaArquivo();
-            } catch (IOException ex) {
-                mostraAlerta(getString(R.string.erro), getString(
-                        R.string.erro_salvando_foto));
-            }
-            if (arquivoFoto != null) {
-                Uri photoURI = FileProvider.getUriForFile(getBaseContext(),
-                        getBaseContext().getApplicationContext().getPackageName() +
-                                ".provider", arquivoFoto);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, CAMERA);
-            }
-        }
-    }
-
-    private void mostraAlerta(String titulo, String mensagem) {
-        AlertDialog alertDialog = new
-                AlertDialog.Builder(PhotoActivity.this).create();
-        alertDialog.setTitle(titulo);
-        alertDialog.setMessage(mensagem);
-        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL,
-                getString(R.string.ok),
-                (dialog, which) -> dialog.dismiss());
-        alertDialog.show();
-    }
-
-    private File criaArquivo() throws IOException {
-        String timeStamp = new
-                SimpleDateFormat("yyyyMMdd_Hhmmss").format(
-                new Date());
-        File pasta = Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES);
-        File imagem = new File(pasta.getPath() + File.separator
-                + "JPG_" + timeStamp + ".jpg");
-        return imagem;
-    }
-
 }
